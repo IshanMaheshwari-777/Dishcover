d967d4a45854aee7e38791fc2295ad3f3ebc1c73
IshanMaheshwari-777 <ishan77maheshwari@gmail.com>
2025-05-03 00:53:47 +0530

    Keyselling Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Searchbar from './Components/Searchbar/Searchbar'
-
+import Carousel from './Components/Carousel/Carousel'
+import KeySelling from './Components/KeySelling/KeySelling'
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Searchbar/>
       <Hero/>
+      <Carousel/>
+      <KeySelling/> 
+
+      
      
     </div>
   )
