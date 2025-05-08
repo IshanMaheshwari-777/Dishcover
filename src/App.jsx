c92f435579e209a58fa3ae1441a526a21ccd27ef
IshanMaheshwari-777 <ishan77maheshwari@gmail.com>
2025-05-08 21:30:50 +0530

    Fixing deployment

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,7 @@ function App() {
       <div>
        
         <Routes>
-    
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
